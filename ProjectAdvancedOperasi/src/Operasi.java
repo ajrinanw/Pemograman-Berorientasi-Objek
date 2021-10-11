79cc714328d31ce296b29c98b4b6cbe9a6a62997
ajrinanw <ajrinanw@gmail.com>
2021-10-11 21:23:30 +0700

    Create Operasi.java

---
+++ ProjectAdvancedOperasi/src/Operasi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Wahyudi
+ */
+public class Operasi {
+    int jumlahkan(int a, int b){
+        return (a+b);
+    }
+    
+    int jumlahkan(int a, int b, int c){
+        return (a+b+c);
+    }
+    
+    double jumlahkan (double a, double b){
+        return (a+b);
+    }
+    
+    double jumlahkan (double a, double b, double c){
+        return (a+b+c);
+    }
+}
