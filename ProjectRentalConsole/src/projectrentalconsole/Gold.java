cb4dc3487f7e6e54627ebec30a0b984d4226f107
ajrinanw <ajrinanw@gmail.com>
2021-11-23 21:52:35 +0700

    Create Gold.java

---
+++ ProjectRentalConsole/src/projectrentalconsole/Gold.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectrentalconsole;
+
+/**
+ *
+ * @author Jey
+ */
+public class Gold extends Silver{
+    int cashback;
+    
+    Gold(){
+        this.biayaRental = 30000;
+        this.disc = 2;
+        this.poin = 5;
+        this.cashback = 5000;
+    }
+}
